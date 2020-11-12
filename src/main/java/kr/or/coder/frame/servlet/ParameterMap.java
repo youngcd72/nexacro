a841ea46bbb117083a37bf98a7496e6c9f08808a
ywkim <ywkim@192.168.200.113>
2020-11-12 23:26:19 +0900

    Indentation 처리 방법 변경

--- src/main/java/kr/or/coder/frame/servlet/ParameterMap.java
+++ src/main/java/kr/or/coder/frame/servlet/ParameterMap.java
@@ -6,35 +6,35 @@ import kr.or.coder.frame.util.NumberUtil;
 
 public class ParameterMap<K, V> extends HashMap<K, V> {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public int getInt(String key) {
+    public int getInt(String key) {
 
-		return NumberUtil.toInt(get(key));
-	}
-	
-	public long getLong(String key) {
-		
-		return NumberUtil.toLong(get(key));
-	}
-	
-	public float getFloat(String key) {
-		
-		return NumberUtil.toFloat(get(key));
-	}
-	
-	public double getDouble(String key) {
-		
-		return NumberUtil.toDouble(get(key));
-	}
-	
-	public String getString(String key) {
-		
-		Object val = get(key);
-		
-		if(val == null) {
-			return "";
-		}
-		return String.valueOf(get(key));
-	}
+        return NumberUtil.toInt(get(key));
+    }
+    
+    public long getLong(String key) {
+        
+        return NumberUtil.toLong(get(key));
+    }
+    
+    public float getFloat(String key) {
+        
+        return NumberUtil.toFloat(get(key));
+    }
+    
+    public double getDouble(String key) {
+        
+        return NumberUtil.toDouble(get(key));
+    }
+    
+    public String getString(String key) {
+        
+        Object val = get(key);
+        
+        if(val == null) {
+            return "";
+        }
+        return String.valueOf(get(key));
+    }
 }
