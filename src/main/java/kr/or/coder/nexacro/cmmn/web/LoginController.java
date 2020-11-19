50453ea1539addcc431ef9a7b590e4f0d6cf35a0
ywkim <ywkim@DESKTOP-VVOJ7PD>
2020-11-19 16:29:06 +0900

    nexacro sample 프로그램 개발

---
+++ src/main/java/kr/or/coder/nexacro/cmmn/web/LoginController.java
@@ -0,0 +1,13 @@
+package kr.or.coder.nexacro.cmmn.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+
+	protected Logger logger = LoggerFactory.getLogger(getClass());
+
+	
+}
