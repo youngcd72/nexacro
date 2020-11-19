f06c21a4ef2f5781786a8ab3de98391ed18ad275
ywkim <ywkim@DESKTOP-VVOJ7PD>
2020-11-19 16:30:08 +0900

    사용자 정의 Exception 추가

--- src/main/java/kr/or/coder/frame/exception/BizException.java
+++ src/main/java/kr/or/coder/frame/exception/BizException.java
@@ -1,5 +1,17 @@
 package kr.or.coder.frame.exception;
 
-public class BizException extends Exception {
+public class BizException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3445081764318715446L;
+
+	public BizException(String msg){
+        super(msg);
+    }
+
+    public BizException(Exception ex){
+        super(ex);
+    }
 }
--- src/main/java/kr/or/coder/frame/exception/UserException.java
+++ src/main/java/kr/or/coder/frame/exception/UserException.java
@@ -1,5 +1,17 @@
 package kr.or.coder.frame.exception;
 
-public class UserException extends Exception {
+public class UserException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8183268733140233808L;
+
+	public UserException(String msg){
+        super(msg);
+    }
+
+    public UserException(Exception ex){
+        super(ex);
+    }
 }
